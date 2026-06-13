c68e5e0ec69d46738c2ae5c40589afcac7f3c6d9
yshanmukasurya <surya23241a05y5@grietcollege.com>
2026-06-13 15:29:17 +0530

    Force Nodemailer to use IPv4 connection for Gmail SMTP to resolve connection unreachable on Render

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ const emailTransporter = nodemailer.createTransport({
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  lookup: (hostname, options, callback) => {
+    options.family = 4;
+    dns.lookup(hostname, options, callback);
   }
 });
 
